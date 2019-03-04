27749a47b1766e1d45668955ca220911d6f8d4d5
Wchunxiao <32158794+Wchunxiao@users.noreply.github.com>
2019-03-04 22:22:36 +0800

    Update FindPathInBT.java

--- code/offer/src/Chap4/FindPathInBT.java
+++ code/offer/src/Chap4/FindPathInBT.java
@@ -1,7 +1,9 @@
 package Chap4;
 
 import java.util.ArrayList;
-
+//二叉树中和为某一路径的值
+//输入一颗二叉树的跟节点和一个整数，打印出二叉树中结点值的和为输入整数的所有路径。
+//路径定义为从树的根结点开始往下一直到叶结点所经过的结点形成一条路径。(注意: 在返回值的list中，数组长度大的数组靠前)
 public class FindPathInBST {
     private class TreeNode {
         int val = 0;
