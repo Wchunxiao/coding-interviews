22eacc4933dea286467c3a707911f970e81634b1
Wchunxiao <32158794+Wchunxiao@users.noreply.github.com>
2019-03-19 16:11:51 +0800

    Update FindGreatestSumOfSubArray.java

--- code/offer/src/Chap5/FindGreatestSumOfSubArray.java
+++ code/offer/src/Chap5/FindGreatestSumOfSubArray.java
@@ -8,8 +8,8 @@ public class FindGreatestSumOfSubArray {
     public int findGreatestSumOfSubArray(int[] array) {
         if (array == null || array.length == 0) return 0;
 
-        int maxSum = array[0];
-        int curSum = array[0];
+        int maxSum = array[0];//最大子数组和
+        int curSum = array[0];//当前累加的子数组和
         for (int i = 1; i < array.length; i++) {
             if (curSum < 0) curSum = array[i];
             else curSum += array[i];
